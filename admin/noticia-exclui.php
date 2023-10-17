7d9cdc26cb4abcd04079ad4d5f56e90c5d533de1
Nicolas_Amorim <ni.oliveiraamorim.25@gmail.com>
2023-10-17 09:18:11 -0300

    controle de acesso

---
+++ admin/noticia-exclui.php
@@ -0,0 +1,7 @@
+<?php
+require_once "../vendor/autoload.php";
+
+use Microblog\ControleDeAcesso;
+
+$sessao  = new ControleDeAcesso;
+$sessao->verificaAcesso();
--- admin/usuario-exclui.php
+++ admin/usuario-exclui.php
@@ -2,8 +2,10 @@
 require_once "../vendor/autoload.php";
 
 use Microblog\Usuario;
+use Microblog\ControleDeAcesso;
 
-
+$sessao  = new ControleDeAcesso;
+$sessao->verificaAcesso();
 
 $usuario = new Usuario;
 $usuario->setId($_GET['id']);
