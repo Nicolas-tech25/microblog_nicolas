3d3ef826436cbb013215b796439c9545ea88ce9f
Nicolas_Amorim <ni.oliveiraamorim.25@gmail.com>
2023-10-18 10:47:33 -0300

    update admin

--- admin/meu-perfil.php
+++ admin/meu-perfil.php
@@ -1,5 +1,30 @@
 <?php 
 require_once "../inc/cabecalho-admin.php";
+use Microblog\Usuario;
+use Microblog\Utilitarios;
+
+$usuario = new Usuario;
+$usuario->setId($_SESSION["id"]);
+
+$dados = $usuario->listarUm();
+// Utilitarios::dump($dados);
+
+if (isset($_POST["atualizar"])) {
+	$usuario->setNome($_POST['nome']);
+    $usuario->setEmail($_POST['email']);
+	$usuario->setTipo($_SESSION['tipo']); // mantendo o tipo existente
+}
+
+if (empty($_POST['senha'])){
+	$usuario->setSenha($dados['senha']);
+}else{
+	$usuario->setSenha(
+		$usuario->verificaSenha($_POST['senha'],$dados['senha'])
+	);
+}
+$usuario->atualizar();
+$_SESSION["nome"] = $usuario->getNome();
+header("location:index.php?perfil_atualizado");
 ?>
 
 
@@ -14,12 +39,12 @@ require_once "../inc/cabecalho-admin.php";
 
 			<div class="mb-3">
 				<label class="form-label" for="nome">Nome:</label>
-				<input class="form-control" type="text" id="nome" name="nome" required>
+				<input class="form-control" value="<?=$dados["nome"]?>" type="text" id="nome" name="nome" required>
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="email">E-mail:</label>
-				<input class="form-control" type="email" id="email" name="email" required>
+				<input class="form-control" value="<?=$dados["email"]?>"  type="email" id="email" name="email" required>
 			</div>
 
 			<div class="mb-3">
