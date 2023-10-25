149202c90f2ca2c79f824cca84042f3c4d8da869
Nicolas_Amorim <ni.oliveiraamorim.25@gmail.com>
2023-10-25 11:54:26 -0300

    atualizar

--- src/noticia.php
+++ src/noticia.php
@@ -73,6 +73,30 @@ final class Noticia{
             return $resultado;
         }
     
+        /* Ler um */
+        public function listarUm():array {
+
+            if ( $this->usuario->getTipo() === "admin" ) {
+                $sql = "SELECT * FROM noticias WHERE id = :id";
+            } else {
+                $sql = "SELECT * FROM noticias WHERE id - :id AND usuario_id = :usuario_id";
+            }
+    
+            try {
+                $consulta = $this->conexao->prepare($sql);
+                $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+
+                if($this->usuario->getTipo() !== "admin"){
+                $consulta->bindValue(":usuario_id", $this->usuario->getId(), PDO::PARAM_INT);
+                }
+                $consulta->execute();
+                $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+            } catch (Exception $erro) {
+                die("Erro ao carregar noticia: ".$erro->getMessage());
+            }
+    
+            return $resultado;
+        }
 
     /* Método para upload de fotos */
     public function upload(array $arquivo):void{
