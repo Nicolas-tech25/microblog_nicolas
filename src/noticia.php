0382bede616f6f5f29eec301797aa197bca4c7b1
Nicolas_Amorim <ni.oliveiraamorim.25@gmail.com>
2023-10-25 10:50:07 -0300

    pagina noticia funcional

--- src/noticia.php
+++ src/noticia.php
@@ -53,7 +53,7 @@ final class Noticia{
         public function listar():array {
 
             if ( $this->usuario->getTipo() === "admin" ) {
-                $sql = "SELECT noticias.id,noticias.titulo, noticias.data,usuarios.nome AS Autor, noticias.destaque  FROM noticias INNER JOIN usuarios ON noticias.usuario_id = usuarios.id  ORDER BY data DESC";
+                $sql = "SELECT noticias.id,noticias.titulo, noticias.data,usuarios.nome AS autor, noticias.destaque  FROM noticias INNER JOIN usuarios ON noticias.usuario_id = usuarios.id  ORDER BY data DESC";
             } else {
                 $sql = "SELECT id, titulo, data, destaque FROM noticias WHERE usuario_id = :usuario_id ORDER BY data DESC";
             }
